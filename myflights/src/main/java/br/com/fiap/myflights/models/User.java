786000c6e041aae2e9755ae2e091e88073c5d4f4
Henry Kinoshita <42702870+PEROLOKO@users.noreply.github.com>
2023-04-13 10:01:03 -0300

    Lombok

--- myflights/src/main/java/br/com/fiap/myflights/models/User.java
+++ myflights/src/main/java/br/com/fiap/myflights/models/User.java
@@ -6,60 +6,29 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity(name = "T_MF_USER")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @NotBlank(message = "Nome é obrigatório")
     private String name;
+
     @NotBlank(message = "Email é obrigatório")
     private String email;
+
     @NotBlank(message = "Senha é obrigatória")
     @Size(min = 5, max = 255)
     private String senha;
 
-    public User() {
-        super();
-    }
 
-    public User(Long id, String name, String email, String senha) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.senha = senha;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
--- myflights/src/main/java/br/com/fiap/myflights/models/Voo.java
+++ myflights/src/main/java/br/com/fiap/myflights/models/Voo.java
@@ -8,9 +8,15 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity(name = "T_MF_VOO")
 public class Voo {
     @Id
@@ -23,61 +29,11 @@ public class Voo {
     @Size(min = 5, max = 255)
     private String destino;
     private Integer portao;
-    
-    public Voo(int numVoo, LocalDateTime horario, String destino, int portao) {
-        this.numVoo = numVoo;
-        this.horario = horario;
-        this.destino = destino;
-        this.portao = portao;
-    }
 
     public Voo(int numVoo, LocalDateTime horario, String destino) {
         this.numVoo = numVoo;
         this.horario = horario;
         this.destino = destino;
-    }
-
-    public Voo() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public int getNumVoo() {
-        return numVoo;
-    }
-
-    public void setNumVoo(int numVoo) {
-        this.numVoo = numVoo;
-    }
-
-    public LocalDateTime getHorario() {
-        return horario;
-    }
-
-    public void setHorario(LocalDateTime horario) {
-        this.horario = horario;
-    }
-
-    public String getDestino() {
-        return destino;
-    }
-
-    public void setDestino(String destino) {
-        this.destino = destino;
-    }
-
-    public int getPortao() {
-        return portao;
-    }
-
-    public void setPortao(int portao) {
-        this.portao = portao;
     }
 
     @Override
